9c2b1d3a64e99c6803bebe9179b04bbf925728d3
Carlos Rabazo <carlosemilio.rabazo@ufv.es>
2022-10-05 16:55:38 +0200

    Subida primera versión de los ficheros

---
+++ Persona.java
@@ -0,0 +1,43 @@
+public class Persona {
+    private String nombre;
+    private String apellidos;
+    private int dniSinLetra;
+    private int edad;
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public String getApellidos(){
+        return apellidos;
+    }
+
+    public int getDniSinLetra(){
+        return dniSinLetra;
+    }
+
+    public int getEdad(){
+        return edad;
+    }
+
+    public void setNombre(String nom){
+        this.nombre = nom;
+    }
+
+    public void setApellidos(String apell){
+        this.apellidos = apell;
+    }
+
+    public void setDniSinLetra(int dni){
+        this.dniSinLetra = dni;
+    }
+
+    public void setEdad(int ed){
+        if (ed >= 18){
+            System.out.println("Estás hecho un chaval");
+        } else {
+            System.out.println("Ya eres talludito");
+        }
+        this.edad = ed;
+    }
+}
